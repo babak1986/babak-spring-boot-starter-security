72931ceae42a701f96b34e45df53e1ad3b798311
babak <behzadi.babak@gmail.com>
2024-08-07 10:35:58 +0330

    init

--- src/main/java/com/babak/springboot/security/utils/CookieUtil.java
+++ src/main/java/com/babak/springboot/security/utils/CookieUtil.java
@@ -1,8 +1,8 @@
 package com.babak.springboot.security.utils;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpHeaders;
 
 import java.util.Arrays;
 
@@ -12,18 +12,29 @@ import java.util.Arrays;
  **/
 public final class CookieUtil {
 
+    private final static int MAX_AGE = 60 * 60 * 24 * 90;
+
     public static String get(HttpServletRequest request, String name) {
         return Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals(name))
                 .findFirst().orElse(null).getValue();
     }
 
-    public static void put(HttpServletResponse response, String name, String value, boolean secure, boolean httpOnly) {
-        String cookie = String.format("%s=%s; secure=%s; httpOnly=%s", name, value, secure, httpOnly);
-        response.addHeader(HttpHeaders.COOKIE, cookie);
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie);
+    public static void put(HttpServletResponse response, String name, String value,
+                           boolean secure, boolean httpOnly, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setSecure(secure);
+        cookie.setHttpOnly(httpOnly);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
     }
 
     public static void token(HttpServletResponse response, String token) {
-        put(response, "b_token", token, true, true);
+        put(response, "b_token", token, true, true, CookieUtil.MAX_AGE);
+    }
+
+    public static void invalidate(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, "");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
